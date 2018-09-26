4f4b1039d1ef190977b76c5d38b48705dafe68ec
chenxin <chenxin@51xf.cn>
2018-09-26 18:59:10 +0800

    wxchart改为echart

--- src/pages/chart/index.vue
+++ src/pages/chart/index.vue
@@ -1,30 +1,39 @@
 <template>
-  <div>
+  <div class="chart">
     <switch-head :period="period" 
       :changeDateCB="changeDate"
       @changeType="type = $event"
     >
     </switch-head>
-    <p-content :type="type" :current="period" :periodType="0">0</p-content>
+    <p-content :type="type" :current="period" :periodType="0" @changeEC="ec0 = $event">0</p-content>
     <p-content :type="type" :current="period" :periodType="1">1</p-content>
     <p-content :type="type" :current="period" :periodType="2">2</p-content>
     
+    {{ec0}}
+    <div class="ec" v-bind:style="{ opacity: activeOpacity0 }"><ec-canvas class="canvas" id="mychart-dom-bar" canvas-id="mychart-bar0" :ec="ec0"></ec-canvas></div>
+    <div class="ec" v-bind:style="{ opacity: activeOpacity1 }"><ec-canvas class="canvas" id="mychart-dom-bar" canvas-id="mychart-bar1" :ec="ec1"></ec-canvas></div>
+    <div class="ec" v-bind:style="{ opacity: activeOpacity2 }"><ec-canvas class="canvas" id="mychart-dom-bar" canvas-id="mychart-bar2" :ec="ec2"></ec-canvas></div>
+        
   </div>
 </template>
 
 <script>
-// var WXCharts = require('../../utils/wxcharts.js')
-
 import head from '../../components/switchHead'
 import './index.scss'
 import content from '../../components/chartContent'
+import { options } from '../../utils/chartUT'
 
-var lineChart = null
 export default {
   data() {
     return {
       period: 0, // 周年月
-      type: 0 // 收入1/支出0
+      type: 0, // 收入1/支出0
+      ec0: options,
+      ec1: options,
+      ec2: options,
+      activeOpacity0: 1,
+      activeOpacity1: 0,
+      activeOpacity2: 0
     }
   },
 
@@ -37,44 +46,33 @@ export default {
     // 修改period周年月
     changeDate({ period }) {
       if (period !== undefined) this.period = period
-    },
-    touchHandler: function(e) {
-      console.log(lineChart.getCurrentDataIndex(e))
-      lineChart.showToolTip(e, {
-        // background: '#7cb5ec',
-        format: function(item, category) {
-          return category + ' ' + item.name + ':' + item.data
-        }
-      })
-    },
-    createSimulationData: function() {
-      var categories = []
-      var data = []
-      for (var i = 0; i < 10; i++) {
-        categories.push('2016-' + (i + 1))
-        data.push(Math.random() * (20 - 10) + 10)
+    }
+  },
+
+  watch: {
+    period: function(prev, next) {
+      switch (next) {
+        case 0:
+          this.activeOpacity0 = 1
+          this.activeOpacity1 = 0
+          this.activeOpacity2 = 0
+          break
+        case 1:
+          this.activeOpacity0 = 0
+          this.activeOpacity1 = 1
+          this.activeOpacity2 = 0
+          break
+        case 2:
+          this.activeOpacity0 = 0
+          this.activeOpacity1 = 0
+          this.activeOpacity2 = 1
+          break
+        default:
+          this.activeOpacity0 = 0
+          this.activeOpacity1 = 0
+          this.activeOpacity2 = 0
+          break
       }
-      // data[4] = null;
-      return {
-        categories: categories,
-        data: data
-      }
-    },
-    updateData: function() {
-      var simulationData = this.createSimulationData()
-      var series = [
-        {
-          name: '成交量1',
-          data: simulationData.data,
-          format: function(val, name) {
-            return val.toFixed(2) + '万'
-          }
-        }
-      ]
-      lineChart.updateData({
-        categories: simulationData.categories,
-        series: series
-      })
     }
   },
 
